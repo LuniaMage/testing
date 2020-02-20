46c84cae0b0615da3b4777c24a6d1998ed9663cf
LuniaMage <luniamage@gmail.com>
2020-02-20 18:56:14 -0500

    second pass - added index-minus-body.html to format top nav menu, sidebar nav, and footer. index.html only has category grid cards.

--- index.js
+++
@@ -1,5 +0,0 @@
-// Show an off-canvas matching the passed CSS selector
-UIkit.offcanvas.show('#my-id');
-
-// Hide any active offcanvas. Set force to true, if you don't want any animation.
-UIkit.offcanvas.hide([force = false])
